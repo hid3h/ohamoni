3458431c40a53c81c870b4c9927d6eb41977c546
hide <razyetts@hotmail.co.jp>
2020-10-17 16:44:32 +0900

    直近8日分を取得

--- app/models/wake_up_time.rb
+++ app/models/wake_up_time.rb
@@ -1,6 +1,10 @@
 class WakeUpTime < ApplicationRecord
   belongs_to :user
 
+  scope :recently, -> { order(woke_up_on: 'DESC') }
+  # 先週の同じ曜日もみたいので実際は8日分
+  scope :last_week, -> { where('woke_up_on <= ?', Time.zone.today).where('woke_up_on >= ?', Time.zone.today - 7.day) }
+
   class << self
     def wake_up(line_user_id:)
       user = User.find_or_create_by_line_user_id(line_user_id: line_user_id)
@@ -8,7 +12,6 @@ class WakeUpTime < ApplicationRecord
       text = 'おはようございます'
       begin
         user.wake_up_times.create(
-          # TODO 日付は日本時間になっているか？
           woke_up_on: Time.zone.today, # https://qiita.com/jnchito/items/cae89ee43c30f5d6fa2c#date%E3%81%AE%E5%A0%B4%E5%90%88
           woke_up_at: Time.zone.now
         )
@@ -17,9 +20,7 @@ class WakeUpTime < ApplicationRecord
       end
 
       # 直近一週間取得
-      to = Time.zone.today
-      from = to - 7.day
-      weekly_data = user.wake_up_times.where(woke_up_on: from...to).order(woke_up_on: 'DESC')
+      weekly_data = user.wake_up_times.last_week.recently
       weekly_data_messages = weekly_data.map do |data|
         '・' + data.woke_up_on.day.to_s + '(' + %w(日 月 火 水 木 金 土)[data.woke_up_on.wday] + ')' + data.woke_up_at.strftime("%H:%M")
       end
