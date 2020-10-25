167b2c610c046fac7e0e99aff2b004c40ddc9db8
hide <razyetts@hotmail.co.jp>
2020-10-25 10:57:52 +0900

    ローカルでなぜか署名が一致しない。本番で一致するか試す

--- app/controllers/api/v1/webhook_controller.rb
+++ app/controllers/api/v1/webhook_controller.rb
@@ -1,15 +1,39 @@
 class Api::V1::WebhookController < ApplicationController
   # https://developers.line.biz/ja/reference/messaging-api/#webhooks
   def receive
-    # TODO 署名チェック
-    
-    MapkunFinder.excuse(events: params['events'])
+    if validate_sinature(channel_secret: mapkun_channel_secret)
+      p "maupu strt"
+      MapkunFinder.excuse(events: params['events'])
+    end
 
-    Finder.excuse(events: params['events'])
+    if validate_sinature(channel_secret: ohamoni_channel_secret)
+      p "oha strt"
+      Finder.excuse(events: params['events'])
+    end
   end
 
   def test
     render :json => "test"
+  end
+
+  private
+
+  def validate_sinature(channel_secret:)
+    http_request_body = request.raw_post # Request body string
+    p "http_request_body", http_request_body
+    hash = OpenSSL::HMAC::digest(OpenSSL::Digest::SHA256.new, channel_secret, http_request_body)
+    signature = Base64.strict_encode64(hash)
+    # Compare X-Line-Signature request header string and the signature
+    p "signature", signature, request.headers['X-Line-Signature']
+    signature == request.headers['X-Line-Signature']
+  end
+
+  def mapkun_channel_secret
+    ENV["LINE_CHANNEL_MAPKUN_SECRET"] || Rails.application.credentials.line[:channel_mapkun_secret]
+  end
+
+  def ohamoni_channel_secret
+    ENV["LINE_CHANNEL_SECRET"] || Rails.application.credentials.line[:channel_secret]
   end
 end
 
