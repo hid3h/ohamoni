97db19162e7d2181f7b33f5ec95d61619c3f9fcf
hid3h <razyetts@hotmail.co.jp>
2023-06-30 14:40:42 +0900

    yarn nest g s prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { LinebotModule } from "./linebot/linebot.module";
 import { ConfigModule } from "@nestjs/config";
+import { PrismaService } from "./prisma/prisma.service";
 
 @Module({
   imports: [ConfigModule.forRoot(), LinebotModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
