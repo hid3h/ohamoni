d1fc8cce145e0ef59d0b19a0d529b2d367e8bc6a
hid3h <razyetts@hotmail.co.jp>
2023-08-04 16:59:45 +0900

    rawBody消しちゃだめ

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    rawBody: true,
+  });
 
   app.useStaticAssets(join(__dirname, "..", "public"));
   app.setBaseViewsDir(join(__dirname, "..", "views"));
