83f728c2911c60e41c61a83ddc93406b74898f1f
hid3h <razyetts@hotmail.co.jp>
2023-08-15 16:58:52 +0900

    技術記事用に

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { LinebotModule } from "./linebot/linebot.module";
 import { ConfigModule } from "@nestjs/config";
 import { ReminderNotificationModule } from "./reminder-notification/reminder-notification.module";
@@ -14,6 +13,5 @@ import { GraphsModule } from "./graphs/graphs.module";
     GraphsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
