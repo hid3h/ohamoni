e0c6fb0e5c27c61ba00a61f24b3630777bc24c45
hid3h <razyetts@hotmail.co.jp>
2023-10-03 17:27:51 +0900

    グラフもsummariesから取得するように!

--- src/getting-up/getting-up.service.ts
+++ src/getting-up/getting-up.service.ts
@@ -1,18 +1,14 @@
 import {
+  ChronoField,
   DateTimeFormatter,
-  Duration,
-  Instant,
-  LocalDateTime,
+  LocalTime,
   Period,
   ZoneId,
-  ZoneOffset,
   ZonedDateTime,
 } from "@js-joda/core";
 import { Client } from "@line/bot-sdk";
 import { Injectable } from "@nestjs/common";
 import { Account } from "@prisma/client";
-import { add, differenceInDays } from "date-fns";
-import { formatInTimeZone } from "date-fns-tz";
 import { AccountsService } from "src/accounts/accounts.service";
 import { PrismaService } from "src/prisma/prisma.service";
 import "@js-joda/timezone";
@@ -45,61 +41,45 @@ export class GettingUpService {
 
     // 1か月前の日本時間を取得
     const oneMonthAgoInJapan = nowInJapan.minus(Period.ofMonths(1));
-
-    // その日の始まりの日本時間を取得
-    const startOfOneMonthAgoInJapan = oneMonthAgoInJapan
-      .toLocalDate()
-      .atStartOfDay(japanZone);
-
-    // UTCに変換
-    const oneMonthAgoInUTC = startOfOneMonthAgoInJapan.withZoneSameInstant(
-      ZoneOffset.UTC,
-    );
-
-    // Date型に変換
-    const oneMonthAgoInUTCDate = new Date(
-      oneMonthAgoInUTC.toInstant().toEpochMilli(),
-    );
-
-    console.log(`Now in Japan: ${nowInJapan}`);
-    console.log(`One month ago in Japan: ${oneMonthAgoInJapan}`);
-    console.log(
-      `Start of one month ago in Japan: ${startOfOneMonthAgoInJapan}`,
-    );
-    console.log(`One month ago in UTC: ${oneMonthAgoInUTC}`);
-    console.log(`One month ago in UTC Date: ${oneMonthAgoInUTCDate}`);
-
-    const gettingUps = await this.fetchGettingUpsByJSTDay({
-      account,
-      fromDate: oneMonthAgoInUTCDate,
+    const jstDate = oneMonthAgoInJapan.format(DateTimeFormatter.ISO_LOCAL_DATE);
+    const gettingUps = await this.prismaService.gettingUpDailySummary.findMany({
+      where: {
+        accountId: account.id,
+        jstDate: {
+          gte: jstDate,
+        },
+      },
     });
 
-    const labels = Object.keys(gettingUps);
-    const data = labels.map((label) => {
-      const gettingUp = gettingUps[label];
-      if (!gettingUp) {
-        return undefined;
-      }
-      const gotUpAtDate = gettingUp.gotUpAt;
-      const gotUpAtInstant = Instant.ofEpochMilli(gotUpAtDate.getTime()); // Instant オブジェクトに変換
-      const gotUpAtInJapan = LocalDateTime.ofInstant(gotUpAtInstant, japanZone); // 日本時間に変換
+    const labels = [];
+    const data = [];
+    // 1ヶ月分を一旦31で固定
+    for (let i = 0; i < 31; i++) {
+      const targetZonedDateTime = oneMonthAgoInJapan.plusDays(i);
 
-      const startOfDayInJapan = gotUpAtInJapan
-        .withHour(0)
-        .withMinute(0)
-        .withSecond(0)
-        .withNano(0); // 日本時間でその日の始まり
+      const targetJstDate = targetZonedDateTime.format(
+        DateTimeFormatter.ISO_LOCAL_DATE,
+      );
+      const targetGettingUp = gettingUps.find((gettingUp) => {
+        return gettingUp.jstDate === targetJstDate;
+      });
 
-      const duration = Duration.between(startOfDayInJapan, gotUpAtInJapan); // 経過時間を計算
-      // 経過時間をミリ秒で取得して返す
-      return duration.toMillis();
-    });
+      labels.push(
+        targetZonedDateTime.format(
+          DateTimeFormatter.ofPattern("dd(E)").withLocale(Locale.JAPAN),
+        ),
+      );
+      const gettingUpSeconds = targetGettingUp
+        ? LocalTime.parse(targetGettingUp.jstTime).get(
+            ChronoField.SECOND_OF_DAY,
+          )
+        : undefined;
+      data.push(gettingUpSeconds);
+    }
 
     return {
-      labels: labels.reverse().map((key) => {
-        return this.replaceDayOfWeekWithJapanese(key.slice(3));
-      }),
-      data: data.reverse(),
+      labels,
+      data,
     };
   }
 
@@ -170,13 +150,12 @@ export class GettingUpService {
     let text = "記録しました！";
 
     const gotUpAtZonedDateTime = ZonedDateTime.parse(gotUpAtStr);
-    const formatterDate = DateTimeFormatter.ofPattern("MM/dd(E)").withLocale(
-      Locale.US,
+    const formattedDate = gotUpAtZonedDateTime.format(
+      DateTimeFormatter.ofPattern("MM/dd(E)").withLocale(Locale.JAPAN),
     );
-    let formattedDate = formatterDate.format(gotUpAtZonedDateTime);
-    formattedDate = this.replaceDayOfWeekWithJapanese(formattedDate);
-    const formatterTime = DateTimeFormatter.ofPattern("HH:mm");
-    const formattedTime = formatterTime.format(gotUpAtZonedDateTime);
+    const formattedTime = gotUpAtZonedDateTime.format(
+      DateTimeFormatter.ofPattern("HH:mm"),
+    );
     text = text + `\n${formattedDate}の起床時間は\n✨${formattedTime}⏱\nです`;
 
     // 記録日時が今日じゃなかったら過去分を返す必要はない
@@ -229,102 +208,5 @@ export class GettingUpService {
       gettingUpRecordMessages.join("\n");
 
     return text;
-  }
-
-  private async fetchGettingUpsByJSTDay({
-    account,
-    fromDate,
-    toDate,
-  }: {
-    account: Pick<Account, "id">;
-    fromDate: Date;
-    toDate?: Date;
-  }) {
-    const accountId = account.id;
-
-    const now = new Date();
-    console.log("now", now);
-    const endDateOfJSTTodayString = formatInTimeZone(
-      now,
-      "Asia/Tokyo",
-      "yyyy-MM-dd'T'14:59:59.999'Z'",
-    );
-    console.log("endDateOfJSTTodayString", endDateOfJSTTodayString);
-    const endDate = toDate ?? new Date(endDateOfJSTTodayString);
-    console.log("endDate", endDate);
-    const gettingUps = await this.prismaService.gettingUp.findMany({
-      where: {
-        accountId,
-        gotUpAt: {
-          gte: fromDate,
-          lte: endDate,
-        },
-      },
-      include: {
-        gettingUpDeletion: true,
-      },
-      orderBy: {
-        registeredAt: "desc",
-      },
-    });
-
-    const gettingUpsWithJSTString = gettingUps.map((gettingUp) => {
-      return {
-        ...gettingUp,
-        gotUpDayJSTString: formatInTimeZone(
-          gettingUp.gotUpAt,
-          "Asia/Tokyo",
-          "MM/dd(E)",
-        ),
-      };
-    });
-
-    const gettingUpMapByJSTDay = new Map<
-      string,
-      (typeof gettingUpsWithJSTString)[0]
-    >();
-    const count = differenceInDays(endDate, fromDate);
-    for (let i = 0; i < count; i++) {
-      const targetDateUTC = add(endDate, { days: -i });
-      console.log("targetDateUTC", targetDateUTC);
-      const targetDayJSTISOString = formatInTimeZone(
-        targetDateUTC,
-        "Asia/Tokyo",
-        "MM/dd(E)",
-      );
-      console.log("targetDayJSTISOString", targetDayJSTISOString);
-      const gettingUp = gettingUpsWithJSTString.find((gettingUp) => {
-        return targetDayJSTISOString === gettingUp.gotUpDayJSTString;
-      });
-
-      if (gettingUp && !gettingUp.gettingUpDeletion) {
-        gettingUpMapByJSTDay[targetDayJSTISOString] = gettingUp;
-      } else {
-        gettingUpMapByJSTDay[targetDayJSTISOString] = undefined;
-      }
-    }
-
-    return gettingUpMapByJSTDay;
-  }
-
-  private replaceDayOfWeekWithJapanese(str) {
-    const daysOfWeek = {
-      Sun: "日",
-      Mon: "月",
-      Tue: "火",
-      Wed: "水",
-      Thu: "木",
-      Fri: "金",
-      Sat: "土",
-    };
-
-    let replacedStr = str;
-
-    for (const day in daysOfWeek) {
-      const regex = new RegExp(day, "g");
-      replacedStr = replacedStr.replace(regex, daysOfWeek[day]);
-    }
-
-    return replacedStr;
   }
 }
