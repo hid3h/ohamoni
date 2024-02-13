ea45af8a637f163a40a21020c72c9d500b498745
hid3h <razyetts@hotmail.co.jp>
2024-02-13 11:35:34 +0900

    deploy

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     rawBody: true,
   });
 
+  // deploy
   app.useStaticAssets(join(__dirname, "..", "public"));
   app.setBaseViewsDir(join(__dirname, "..", "views"));
   app.setViewEngine("hbs");
